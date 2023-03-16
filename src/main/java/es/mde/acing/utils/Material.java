c0ccdbe317112e98663bfeea734f9b6ba30b06c3
samot <samotcarrasco@gmail.com>
2023-03-16 09:42:55 +0100

    añadida clase Material

---
+++ src/main/java/es/mde/acing/utils/Material.java
@@ -0,0 +1,12 @@
+package es.mde.acing.utils;
+
+public class Material {
+	private int id;
+	private String nombre;
+	
+	public Material (int id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+}
